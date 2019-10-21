553495d90a1cb3d66a6613f35eb95a79841b8e5a
GY <gayoung1998@gmail.com>
2019-10-21 20:35:53 +0900

    modify calc distance function

--- code/GetWifiInfo.js
+++ code/GetWifiInfo.js
@@ -1,19 +1,3 @@
-// 참조 (https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula)
-function getDistance(lat1,lon1,lat2,lon2) {
-  const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2-lat1);
-  const dLon = deg2rad(lon2-lon1); 
-  const a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  const d = R * c; // Distance in km
-  
-  return d;
-}
-
 function computeDistance(lat1,lon1,lat2,lon2) {
     var startLatRads = degreesToRadians(lat1);
     var startLongRads = degreesToRadians(lon1);
@@ -33,16 +17,10 @@ function degreesToRadians(degrees) {
     return radians;
 }
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
 module.exports.function = function getWifiInfo (near, point, self) {
   const dummyData = require("./data/Wifi_data.js");
   const console = require('console');
-  
-  //console.log(self);
-  
+    
   let result = [];
   
   if(near != undefined){
